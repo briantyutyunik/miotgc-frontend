c1b503d572f2033a6351e082ab2f11012f636ff2
Brian Tyutyunik <briantyutyunik@gmail.com>
2023-01-30 20:43:30 -0500

    Merge

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Brian Merge Thing</Text>
+      <Text>Brian Merge Thing Again</Text>
       <StatusBar style="auto" />
     </View>
   );
