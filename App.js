852e438d0398011d78a6f9b7f924fe9845e83d27
Brian Tyutyunik <briantyutyunik@gmail.com>
2023-01-30 20:37:58 -0500

    brian

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>TESTING</Text>
+      <Text>Brian</Text>
       <StatusBar style="auto" />
     </View>
   );
