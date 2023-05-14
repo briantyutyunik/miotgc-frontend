d4028d399b3413870008eae1581b6f2cce7414a0
brooky11 <jonathanyashayev11@gmail.com>
2023-05-14 04:34:24 -0400

    oops

--- util/api/openaiApi.js
+++ util/api/openaiApi.js
@@ -8,7 +8,7 @@ import Constants from "expo-constants";
 const apiKey = Constants.manifest.extra.openaiApiKey;
 
 const configuration = new Configuration({
-  apiKey: "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" ,
+  apiKey: "" ,
 });
 const openai = new OpenAIApi(configuration);
 
